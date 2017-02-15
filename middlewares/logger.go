b608f17313b820682c4460b6d927016f40d36627
franek <franek.richardson@digital.hmrc.gov.uk>
2017-02-15 09:00:58 +0000

    TXMNT-606 Added JSON loggin PR and modified for mdtp

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -74,14 +74,52 @@ type logEntry struct {
 	Host          string `json:"host"`
 }
 
+// logEntry is a single log entry for use in encoding to json
+// general rules:
+//   http_{NAME} the request header NAME - lowercased with underscores replacing spaces
+//   sent_http_{NAME} the response header NAME - lowercased with underscores replacing spaces
+//   upstream_http_{NAME} the server response header NAME - lowercased with underscores replacing spaces
+type mdtpLogEntry struct {
+	BodyBytesSent          string `json:"body_bytes_sent"`		//number of bytes sent to a client, not counting the response header; this variable is compatible with the “%B” parameter of the mod_log_config Apache module
+	Connection             string `json:"connection"`		//connection serial number (1.3.8, 1.2.5)
+	BytesSent              string `json:"bytes_sent"`		//the number of bytes sent to a client
+	GzipRatio              string `json:"gzip_ratio"`		//log the achieved compression ratio.
+	HttpHost               string `json:"http_host"`		//The value of the HTTP request header HEADER when converted to lowercase and with 'dashes' converted to 'underscores', e.g. $http_user_agent, $http_referer...;
+	HttpReferrer           string `json:"http_referrer"`		//referer header (please note the log entry has the correct spelling as opposed to the actual header)
+	HttpUserAgent          string `json:"http_user_agent"`		//user_agent request header
+	HttpXRequestChain      string `json:"http_x_request_chain"`	//x_request_chain request header
+	HttpXSessionId         string `json:"http_x_session_id"`	//x_session_id request header
+	HttpXRequestId         string `json:"http_x_request_id"`	//x_request_id request header
+	RemoteAddr             string `json:"remote_addr"`		//client address
+	HttpTrueClientIp       string `json:"http_true_client_ip"`	//true_client_ip header
+	ProxyHost              string `json:"proxy_host"`		//name and port of a proxied server as specified in the proxy_pass directive
+	RemoteUser             string `json:"remote_user"`		//user name supplied with the Basic authentication
+	Request                string `json:"request"`			//full original request line
+	RequestMethod          string `json:"request_method"`		//request method, usually “GET” or “POST”
+	RequestTime            string `json:"request_time"`		//request processing time in seconds with a milliseconds resolution (1.3.9, 1.2.6); time elapsed since the first bytes were read from the client
+	RequestLength          string `json:"request_length"`		//request length (including request line, header, and request body) (1.3.12, 1.2.7)
+	SentHttpLocation       string `json:"sent_http_location"`	//location response header
+	ServerName             string `json:"server_name"`		//name of the server which accepted a request
+	ServerPort             string `json:"server_port"`		//port of the server which accepted a request
+	Status                 string `json:"status"`			//response status (1.3.2, 1.2.2)
+	TimeLocal              string `json:"time_local"`		//local time in the Common Log Format (1.3.12, 1.2.7)
+	UpstreamAddr           string `json:"upstream_addr"`		//keeps the IP address and port, or the path to the UNIX-domain socket of the upstream server. If several servers were contacted during request processing, their addresses are separated by commas, e.g. “192.168.1.1:80, 192.168.1.2:80, unix:/tmp/sock”. If an internal redirect from one server group to another happens, initiated by “X-Accel-Redirect” or error_page, then the server addresses from different groups are separated by colons, e.g. “192.168.1.1:80, 192.168.1.2:80, unix:/tmp/sock : 192.168.10.1:80, 192.168.10.2:80”.
+	UpstreamHttpProxyAgent string `json:"upstream_http_proxy_agent"` //proxy_agent response header
+	UpstreamHttpServer     string `json:"upstream_http_server"`	//server response header
+	UpstreamResponseLength string `json:"upstream_response_length"`	//keeps the length of the response obtained from the upstream server (0.7.27); the length is kept in bytes. Lengths of several responses are separated by commas and colons like addresses in the $upstream_addr variable.
+	UpstreamResponseTime   string `json:"upstream_response_time"`	//keeps time spent on receiving the response from the upstream server; the time is kept in seconds with millisecond resolution. Times of several responses are separated by commas and colons like addresses in the $upstream_addr variable.
+	UpstreamStatus         string `json:"upstream_status"`		//keeps status code of the response obtained from the upstream server. Status codes of several responses are separated by commas and colons like addresses in the $upstream_addr variable.
+	HttpXForwardedFor      string `json:"x_forwarded_for"`		//x_forwarded_for request header
+}
+
 // logEntryPool is used as we allocate a new logEntry on every request
 var logEntryPool = sync.Pool{
 	New: func() interface{} {
-		return &logEntry{}
+		return &mdtpLogEntry{}
 	},
 }
 
-func (fblh *frontendBackendLoggingHandler) writeJSON(e *logEntry) {
+func (fblh *frontendBackendLoggingHandler) writeJSON(e *mdtpLogEntry) {
 	data, err := json.Marshal(e)
 	if err != nil {
 		log.Error("unable to marshal json for log entry", err)
@@ -92,9 +130,9 @@ func (fblh *frontendBackendLoggingHandler) writeJSON(e *logEntry) {
 	fblh.writer.Write(data)
 }
 
-func (fblh *frontendBackendLoggingHandler) writeText(e *logEntry) {
-	fmt.Fprintf(fblh.writer, `%s - %s [%s] "%s %s %s" %d %d "%s" "%s" %s "%s" "%s" %dms%s`,
-		e.Host, e.Username, e.Timestamp, e.Method, e.URI, e.Protocol, e.Status, e.Size, e.Referer, e.UserAgent, e.RequestID, e.Frontend, e.Backend, e.ElapsedMillis, "\n")
+func (fblh *frontendBackendLoggingHandler) writeText(e *mdtpLogEntry) {
+	fmt.Fprintf(fblh.writer, `%s - %s [%s] "%s" %d %d "%s" "%s" %s "%s" "%s" %dms%s`,
+		e.HttpHost, e.RemoteUser, e.TimeLocal, e.Request, e.Status, e.RequestLength, e.HttpReferrer, e.HttpUserAgent, e.Connection, "-", "-", e.RequestTime, "\n")
 }
 
 // newLineByte is simple "\n" as a byte
@@ -168,9 +206,15 @@ func (fblh *frontendBackendLoggingHandler) ServeHTTP(rw http.ResponseWriter, req
 		}
 	}
 
-	host, _, err := net.SplitHostPort(req.RemoteAddr)
+	ip, _, err := net.SplitHostPort(req.RemoteAddr)
 	if err != nil {
-		host = req.RemoteAddr
+		ip = req.RemoteAddr
+	}
+
+	host, port, err := net.SplitHostPort(req.Host)
+	if err != nil {
+		host = req.Host
+		port = "80"
 	}
 
 	uri := url.RequestURI()
@@ -178,24 +222,55 @@ func (fblh *frontendBackendLoggingHandler) ServeHTTP(rw http.ResponseWriter, req
 		uri = uri[0:qmIndex]
 	}
 
-	e := logEntryPool.Get().(*logEntry)
+	e := logEntryPool.Get().(*mdtpLogEntry)
 	defer logEntryPool.Put(e)
+	time.Since(startTime).Seconds()
 
-	e.RemoteAddr = host
-	e.Username = username
-	e.Timestamp = startTime.Format("02/Jan/2006:15:04:05 -0700")
-	e.Method = req.Method
-	e.URI = uri
-	e.Protocol = req.Proto
-	e.Status = infoRw.GetStatus()
-	e.Size = infoRw.GetSize()
-	e.Referer = req.Referer()
-	e.UserAgent = req.UserAgent()
-	e.RequestID = fblh.reqid
-	e.Frontend = strings.TrimPrefix(infoRw.GetFrontend(), "frontend-")
-	e.Backend = infoRw.GetBackend()
-	e.ElapsedMillis = time.Since(startTime).Nanoseconds() / 1000000
-	e.Host = req.Host
+	e.BodyBytesSent = strconv.Itoa(infoRw.GetSize())
+	e.Connection = fblh.reqid
+	e.BytesSent = strconv.Itoa(infoRw.GetSize()) //todo - difference between this and body bytes sent
+	e.GzipRatio = "-" //todo calculate gzip ratio
+	e.HttpHost = req.Host
+	e.HttpReferrer = req.Referer()
+	e.HttpUserAgent = req.UserAgent()
+	e.HttpXRequestChain = req.Header.Get("X-Request-Chain")
+	e.HttpXSessionId = req.Header.Get("X-Session-ID")
+	e.HttpXRequestId = req.Header.Get("X-Request-ID")
+	e.RemoteAddr = ip
+	e.HttpTrueClientIp = req.Header.Get("True-Client-IP")
+	e.ProxyHost = infoRw.backend
+	e.RemoteUser = username
+	e.Request = fmt.Sprintf("%s %s %s", req.Method, req.RequestURI, req.Proto) 
+	e.RequestMethod = req.Method
+	e.RequestTime = strconv.FormatFloat(time.Since(startTime).Seconds(), 'f', 3, 64)
+	e.RequestLength = "-" //todo request length
+	e.SentHttpLocation = rw.Header().Get("Location")
+	e.ServerName = host
+	e.ServerPort = port
+	e.Status = strconv.Itoa(infoRw.GetStatus())
+	e.TimeLocal = startTime.Format("02/Jan/2006:15:04:05 -0700")
+	e.UpstreamAddr = "-" //todo get ip of actual backend used
+	e.UpstreamHttpProxyAgent = "-" //todo
+	e.UpstreamHttpServer = "-" //todo
+	e.UpstreamResponseLength = "-" //todo
+	e.UpstreamResponseTime = "-" //todo
+	e.UpstreamStatus = "-" //todo
+	e.HttpXForwardedFor = req.Header.Get("X-Forwarded-For")
+
+	//e.Username = username
+	//e.Timestamp = startTime.Format("02/Jan/2006:15:04:05 -0700")
+	//e.Method = req.Method
+	//e.URI = uri
+	//e.Protocol = req.Proto
+	//e.Status = infoRw.GetStatus()
+	//e.Size = infoRw.GetSize()
+	//e.Referer = req.Referer()
+	//e.UserAgent = req.UserAgent()
+	//e.RequestID = fblh.reqid
+	//e.Frontend = strings.TrimPrefix(infoRw.GetFrontend(), "frontend-")
+	//e.Backend = infoRw.GetBackend()
+	//e.ElapsedMillis = time.Since(startTime).Nanoseconds() / 1000000
+	//e.Host = req.Host
 
 	if fblh.format == "json" {
 		fblh.writeJSON(e)
